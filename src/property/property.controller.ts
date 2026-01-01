4f2b144a4ac53dbe651c19e6a014e19f70311176
Guhirwa <christianguhirwa74@gmail.com>
2026-01-01 21:27:27 +0200

    feat(property): customize Http Status Code
    - using @HttpCode annotation / decorator we can be a ble to override the default status generated by nestJs

--- src/property/property.controller.ts
+++ src/property/property.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
 
 @Controller('property')
 export class PropertyController {
   @Get()
+  @HttpCode(200)
   findAll() {
     return 'All properties';
   }
 
   @Post()
+  @HttpCode(202)
   create(@Body('id') id: number, @Body('name') name: string) {
     return {
       success: true,
@@ -18,6 +20,7 @@ export class PropertyController {
   }
 
   @Get(':id/:slug')
+  @HttpCode(200)
   findOne(@Param('id') id: number, @Param('slug') slug: string) {
     return {
       success: true,
